c0b6386925d404690513ad6e0cb900c765ff4d82
Asi <asierrosa@gmail.com>
2021-02-19 11:02:14 +0100

    amaituta

--- google_img_web_scraping.py
+++ google_img_web_scraping.py
@@ -1,4 +1,6 @@
 # -*- coding: UTF-8 -*-   #para poder meter caracteres especiales
+import base64
+
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -17,9 +19,8 @@ browser = webdriver.Firefox()
 # abrir la pagina
 browser.get(uri)
 # esperar hasta que se haya renderizado el elemento que nos interesa (timeout=30s)
-
 #como tal el nombre de la imagen tiene que tener un espacio pero este se reemplaza con un punto
-WebDriverWait(browser, 30).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "rg_i.Q4LuWd.Q4LuWd")))
+WebDriverWait(browser, 30).until(EC.presence_of_all_elements_located((By.CLASS_NAME, "rg_i.Q4LuWd")))
 # obtener el código HTML
 html = browser.page_source
 # cerrar el navegador
@@ -28,26 +29,30 @@ browser.close()
 # instanciar un parser para html
 # y cargar en memoria el DOM del html
 # "soup" es una ref. al elemento raíz del DOM
-soup = BeautifulSoup(html, 'html.parser')
+soup = BeautifulSoup(html, 'html.parser')     #soup HTML-aren document erroreari erreferentzia egiten dio
+
 # buscar en el DOM todos aquellos elementos
 # cuyo atributo "class" valga "rg_i Q4LuWd tx8vtf"
-img_results = soup.find_all('img', {'class': 'rg_i Q4LuWd tx8vtf'})
+img_results = soup.find_all('img', {'class': 'rg_i Q4LuWd'}) #documentuan interesatzen zaizkigun "img"-ak bilatu,
+                                                                    #kasu honetan espazioa jarri egiten da
+
+#ya que img_results es un array podemos iterarlo
 for idx, each in enumerate(img_results):
     src = ""
     if each.has_attr('src'):
-        src = each['src']
+        src = each['src']       #te mete los src-s de las imagenes iteradas en el src, dentro puede tener un base64
+                                #o una uri
     else:
         src = each['data-src']
 
-    img_link = ""
+    img = None
     if src.find("data:image") != -1:
-        img_link = each['data-iurl']
+        img = base64.b64decode(src.replace("data:image/jpeg;base64,", ""))
+        img = base64.b64decode(src.replace("data:image/png;base64,", ""))
     else:
-        img_link = src
-    print(idx, img_link)
+        erantzuna = requests.get(src)
+        img = erantzuna.content
 
-    res = requests.get(img_link)
-    img = res.content
     file = open("./img/" + str(idx) + ".jpeg", "wb")
     file.write(img)
     file.close()
